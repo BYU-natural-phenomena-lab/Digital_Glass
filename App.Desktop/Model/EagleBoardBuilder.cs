a738d85a326a23ab37f15e92c239190796140759
Nate McMaster <nate.mcmaster@byu.edu>
2015-02-23 15:14:26 -0700

    Add eagle board data model

---
+++ App.Desktop/Model/EagleBoardBuilder.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+
+namespace Walle.Model
+{
+    public class EagleBoardBuilder
+    {
+        private readonly EagleBoard _board = new EagleBoard();
+        public EagleBoard.Element Pinhead { get; private set; }
+        private readonly IList<EagleBoard.Element> _leds = new List<EagleBoard.Element>();
+
+        /// <summary>
+        /// Pin 1 = GND
+        /// Pin 2 = VCC
+        /// Pin 3 = Data IN
+        /// </summary>
+        public EagleBoardBuilder()
+        {
+            _board.Signals.Add("GND", new EagleBoard.Signal());
+            _board.Signals.Add("VCC", new EagleBoard.Signal());
+            Pinhead = new EagleBoard.Element
+            {
+                Name = "LP1",
+                Package = new Pinhead3Rot90(),
+                X = 10,
+                Y = 10,
+                Rotation = 0
+            };
+            _board.Elements.Add(Pinhead);
+            _board.Signals["GND"].AddContact(
+                element: Pinhead,
+                padIndex: 1 // GND
+                );
+            _board.Signals["VCC"].AddContact(
+                element: Pinhead,
+                padIndex: 2 // VCC
+                );
+        }
+
+        public IReadOnlyList<EagleBoard.Element> Leds
+        {
+            get { return new ReadOnlyCollection<EagleBoard.Element>(_leds); }
+        }
+
+
+        public void SetPinheadLocation(double x, double y)
+        {
+            Pinhead.X = x;
+            Pinhead.Y = y;
+        }
+
+        public void AddLedAtPoint(double x, double y)
+        {
+            var newLed = new EagleBoard.Element
+            {
+                Name = "LED" + (_leds.Count),
+                Package = new WS2812B(),
+                X = x,
+                Y = y
+            };
+
+            var ledSignal = new EagleBoard.Signal();
+            if (_leds.Count == 0)
+            {
+                ledSignal.AddContact(
+                    element: Pinhead,
+                    padIndex: 3 // Data
+                    );
+            }
+            else
+            {
+                ledSignal.AddContact(
+                    element: _leds.Last(),
+                    padIndex: 2 // DOUT
+                    );
+            }
+            ledSignal.AddContact(
+                element: newLed,
+                padIndex: 4 // DIN
+                );
+
+            _leds.Add(newLed);
+            _board.Elements.Add(newLed);
+
+            _board.Signals.Add("N$" + newLed.Name, ledSignal);
+            _board.Signals["VCC"].AddContact(
+                element: newLed,
+                padIndex: 1 // VCC
+                );
+            _board.Signals["GND"].AddContact(
+                element: newLed,
+                padIndex: 3 // GND
+                );
+        }
+    }
+}
---
+++ App.Desktop/Model/Pinhead3Rot90.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Walle.Model
+{
+    public class Pinhead3Rot90 : EagleBoard.IPackage
+    {
+        private readonly IList<string> _pads = new[]
+        {
+            "1",
+            "2",
+            "3",
+        };
+
+        public string PackageName
+        {
+            get { return "1X03/90"; }
+        }
+
+        public IList<string> Pads
+        {
+            get { return _pads; }
+        }
+
+        public string WiresXml()
+        {
+            return @"
+<wire x1=""-3.81"" y1=""-1.905"" x2=""-1.27"" y2=""-1.905"" width=""0.1524"" layer=""21""/>
+<wire x1=""-1.27"" y1=""-1.905"" x2=""-1.27"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""-1.27"" y1=""0.635"" x2=""-3.81"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""-3.81"" y1=""0.635"" x2=""-3.81"" y2=""-1.905"" width=""0.1524"" layer=""21""/>
+<wire x1=""-2.54"" y1=""6.985"" x2=""-2.54"" y2=""1.27"" width=""0.762"" layer=""21""/>
+<wire x1=""-1.27"" y1=""-1.905"" x2=""1.27"" y2=""-1.905"" width=""0.1524"" layer=""21""/>
+<wire x1=""1.27"" y1=""-1.905"" x2=""1.27"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""1.27"" y1=""0.635"" x2=""-1.27"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""0"" y1=""6.985"" x2=""0"" y2=""1.27"" width=""0.762"" layer=""21""/>
+<wire x1=""1.27"" y1=""-1.905"" x2=""3.81"" y2=""-1.905"" width=""0.1524"" layer=""21""/>
+<wire x1=""3.81"" y1=""-1.905"" x2=""3.81"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""3.81"" y1=""0.635"" x2=""1.27"" y2=""0.635"" width=""0.1524"" layer=""21""/>
+<wire x1=""2.54"" y1=""6.985"" x2=""2.54"" y2=""1.27"" width=""0.762"" layer=""21""/>
+<pad name=""1"" x=""-2.54"" y=""-3.81"" drill=""1.016"" shape=""long"" rot=""R90""/>
+<pad name=""2"" x=""0"" y=""-3.81"" drill=""1.016"" shape=""long"" rot=""R90""/>
+<pad name=""3"" x=""2.54"" y=""-3.81"" drill=""1.016"" shape=""long"" rot=""R90""/>
+<text x=""-4.445"" y=""-3.81"" size=""1.27"" layer=""25"" ratio=""10"" rot=""R90"">&gt;NAME</text>
+<text x=""5.715"" y=""-3.81"" size=""1.27"" layer=""27"" rot=""R90"">&gt;VALUE</text>
+<rectangle x1=""-2.921"" y1=""0.635"" x2=""-2.159"" y2=""1.143"" layer=""21""/>
+<rectangle x1=""-0.381"" y1=""0.635"" x2=""0.381"" y2=""1.143"" layer=""21""/>
+<rectangle x1=""2.159"" y1=""0.635"" x2=""2.921"" y2=""1.143"" layer=""21""/>
+<rectangle x1=""-2.921"" y1=""-2.921"" x2=""-2.159"" y2=""-1.905"" layer=""21""/>
+<rectangle x1=""-0.381"" y1=""-2.921"" x2=""0.381"" y2=""-1.905"" layer=""21""/>
+<rectangle x1=""2.159"" y1=""-2.921"" x2=""2.921"" y2=""-1.905"" layer=""21""/>
+";
+        }
+    }
+}
